66c03ef2d1c14144d6f73e7fc8d424675f2712de
ronye <ronye.rocha@gmail.com>
2023-02-22 16:08:27 -0300

    adicionando filtros

--- confin.api/Program.cs
+++ confin.api/Program.cs
@@ -8,11 +8,13 @@ using FluentValidation.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers()
-    .AddJsonOptions(opt => 
-    {
-        opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
+builder.Services.AddControllers(opt => 
+{
+    opt.Filters.Add(typeof(CompraExceptionFilter));
+}).AddJsonOptions(opt => 
+{
+    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
