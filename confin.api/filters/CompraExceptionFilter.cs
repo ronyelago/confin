62ac2a2e81d808574231655452318085e083fde6
ronye <ronye.rocha@gmail.com>
2023-02-21 22:35:24 -0300

    filter

---
+++ confin.api/filters/CompraExceptionFilter.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace confin.api.filters
+{
+    public class CompraExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            
+        }
+    }
+}
