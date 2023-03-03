58c3f275a464cd2e75e3e5521ad1a9b869bfebdb
ronye <ronye.rocha@gmail.com>
2023-03-02 22:22:04 -0300

    postres str

--- confin.api/validators/NovaCompraValidator.cs
+++ confin.api/validators/NovaCompraValidator.cs
@@ -10,9 +10,9 @@ namespace confin.api.validators
             RuleLevelCascadeMode = CascadeMode.Stop;
 
             RuleFor(x => x.Descricao).NotEmpty().Length(3).MaximumLength(64);
-            RuleFor(x => x.Valor).NotEmpty().GreaterThan(0).Must();
+            RuleFor(x => x.Valor).NotEmpty().GreaterThan(0);
             RuleFor(x => x.FormaPagamento).NotEmpty();
-            RuleFor(x => x.DataCompra).LessThanOrEqualTo(DateTime.Now);//
+            RuleFor(x => x.DataCompra).LessThanOrEqualTo(DateTime.Now);
         }
     }
 }
