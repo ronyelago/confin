1106aa5ade4ec79a60d98ce3e98093dfa4b91d07
ronye <ronye.rocha@gmail.com>
2023-03-06 23:54:39 -0300

    labigula

--- confin.api/Controllers/CompraController.cs
+++ confin.api/Controllers/CompraController.cs
@@ -27,7 +27,7 @@ namespace confin.Controllers
         [HttpGet("ObterTodasCompras")]
         public async Task<IActionResult> Get()
         {
-            Log.Information("*****Obtendo todas as compra manow...*****");
+            Log.Information("*****Obtendo todas as compra labigula...*****");
             var compras = await _compraRepository.Get();
 
             return Ok(compras);
