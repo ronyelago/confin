214c8cc8585433629d43f7ea53d0f0f89b30fb17
ronye <ronye.rocha@gmail.com>
2023-03-10 12:03:26 -0300

    get cards

--- confin.api/Controllers/CompraController.cs
+++ confin.api/Controllers/CompraController.cs
@@ -33,6 +33,13 @@ namespace confin.Controllers
             return Ok(compras);
         }
 
+        [HttpGet("ObterCartao")]
+        public async Task<IActionResult> GetCards()
+        {
+
+            return Ok(new { number = 01111987 });
+        }
+
         [HttpPost("NovaCompra")]
         public async Task<IActionResult> Post([FromBody] NovaCompraModel compraModel)
         {
