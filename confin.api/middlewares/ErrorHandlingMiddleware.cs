68dda45714f45eebc6bb86c5c82d637dd8bab9fc
ronye <ronye.rocha@gmail.com>
2023-03-10 11:29:32 -0300

    pumblas

--- confin.api/middlewares/ErrorHandlingMiddleware.cs
+++ confin.api/middlewares/ErrorHandlingMiddleware.cs
@@ -37,7 +37,7 @@ namespace confin.api.middlewares
             return context.Response.WriteAsync(new ErrorDetails
             {
                 StatusCode = context.Response.StatusCode,
-                Message = $"Deu ruim, pumblas => { exception?.Message }; {exception?.InnerException?.Message}"
+                Message = $"Deu ruim, pumblas.. => { exception?.Message }; {exception?.InnerException?.Message}"
             }.ToString());
         }
     }
