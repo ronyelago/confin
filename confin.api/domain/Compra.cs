bcf5aa8c4b6eb9686a9d37fc95e5e1fd6c349cdc
ronye <ronye.rocha@gmail.com>
2023-03-12 18:25:52 -0300

    parcelada" \

--- confin.api/domain/Compra.cs
+++ confin.api/domain/Compra.cs
@@ -8,7 +8,7 @@ namespace confin.domain
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
         public FormaPagamento FormaPagamento { get; set; }
-        public bool Parcelado { get; set; }
+        public bool Parcelada { get; set; }
         public DateTime DataCompra { get; set; }
     }
 
