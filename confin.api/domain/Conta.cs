97664e3beab4001182dce30b9dacfc17a4a932a8
ronye <ronye.rocha@gmail.com>
2023-03-13 20:53:27 -0300

    variabilidade de conta

--- confin.api/domain/Conta.cs
+++ confin.api/domain/Conta.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
+        public Int16 Variabilidade { get; set; }
         public DateTime Vencimento { get; set; }
     }
 }
