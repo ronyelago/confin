69180f4ccb65d14789d6a2d05350b9a8ce50ff41
ronye <ronye.rocha@gmail.com>
2023-03-15 12:14:09 -0300

    ajustando entidade e model

--- confin.api/domain/Conta.cs
+++ confin.api/domain/Conta.cs
@@ -6,6 +6,8 @@
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
         public Int16 Variabilidade { get; set; }
+        public string Observacoes { get; set; }
+        public int? Status { get; set; }
         public DateTime Vencimento { get; set; }
     }
 }
--- confin.api/models/NovaContaModel.cs
+++ confin.api/models/NovaContaModel.cs
@@ -5,6 +5,8 @@ namespace confin.api.models
         public string Descricao { get; set; }
         public decimal Valor { get; set; }
         public Int16 Variabilidade { get; set; }
+        public string Observacoes { get; set; }
+        public int? Status { get; set; }
         public DateTime Vencimento { get; set; }
     }
 }
