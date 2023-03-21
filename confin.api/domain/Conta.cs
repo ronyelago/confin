43133bd52697f770af1a8ffedfebd891095111f9
ronye <ronye.rocha@gmail.com>
2023-03-21 19:11:45 -0300

    ContaPagar entity

--- confin.api/domain/Conta.cs
+++ confin.api/domain/Conta.cs
@@ -7,6 +7,7 @@
         public Int16 Variabilidade { get; set; }
         public string Observacoes { get; set; }
         public DateTime Vencimento { get; set; }
+        public DateTime DataExpiracao { get; set; }
         public List<ContaPagar> ContasPagar { get; set; }
     }
 }
