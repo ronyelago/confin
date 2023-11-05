5f89af52ceee6d2643433001e0571b2f26b41dfe
Ronye Cornetti <ronye.rocha@gmail.com>
2023-11-05 15:00:20 -0300

    refactor

--- Confin.Domain/Entities/ContaPagar.cs
+++ Confin.Domain/Entities/ContaPagar.cs
@@ -1,21 +1,19 @@
-namespace Confin.Domain.Entities
+namespace Confin.Domain.Entities;
+public class ContaPagar
 {
-    public class ContaPagar
-    {
-        public int Id { get; set; }
-        public int CadastroContaId { get; set; }
-        public Conta CadastroConta { get; set; }
-        public decimal Valor { get; set; }
-        public DateTime DataPagamento { get; set; }        
-        public DateTime DataVencimento { get; set; }
-        public StatusConta Status { get; set; }
-    }
+    public int Id { get; set; }
+    public int CadastroContaId { get; set; }
+    public Conta CadastroConta { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime DataPagamento { get; set; }        
+    public DateTime DataVencimento { get; set; }
+    public StatusConta Status { get; set; }
+}
 
-    public enum StatusConta
-    {
-        Pendente = 1,
-        Paga = 2,
-        Atrasada = 3,
-        PagaAtrasada = 4
-    }
+public enum StatusConta
+{
+    Pendente = 1,
+    Paga = 2,
+    Atrasada = 3,
+    PagaAtrasada = 4
 }
