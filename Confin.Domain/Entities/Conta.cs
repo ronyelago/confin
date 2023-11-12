e95b5567e559b0f17d6f6c66d0c6bc61993549a5
Ronye Cornetti <ronye.rocha@gmail.com>
2023-11-12 15:00:29 -0300

    refactor

--- Confin.Domain/Entities/Conta.cs
+++ Confin.Domain/Entities/Conta.cs
@@ -1,30 +1,28 @@
-﻿namespace Confin.Domain.Entities
-{
-    public class Conta
-    {   
-        public Conta()
-        {
-            ContasPagar = new List<ContaPagar>();
-        }
-
-        public int Id { get; set; }
-        public string? Descricao { get; set; }
-        public decimal ValorMedio {get; set; } = 0;
-        public Variabilidade Variabilidade { get; set; }
-        public bool Ativa { get; set; }
-        public string? Observacoes { get; set; }
-        public int DiaVencimento { get; set; }
-        public DateTime? DataCadastro { get; set; }
-        public DateTime? DataExpiracao { get; set; }
-        public List<ContaPagar> ContasPagar { get; set; }
-    }
-
-    public enum Variabilidade
+﻿namespace Confin.Domain.Entities;
+public class Conta
+{   
+    public Conta()
     {
-        Invariavel = 0,
-        Baixa = 1,
-        Media = 2,
-        Alta = 3,
-        Altissima = 4
+        ContasPagar = new List<ContaPagar>();
     }
+
+    public int Id { get; set; }
+    public string? Descricao { get; set; }
+    public decimal ValorMedio {get; set; } = 0;
+    public Variabilidade Variabilidade { get; set; }
+    public bool Ativa { get; set; }
+    public string? Observacoes { get; set; }
+    public int DiaVencimento { get; set; }
+    public DateTime? DataCadastro { get; set; }
+    public DateTime? DataExpiracao { get; set; }
+    public List<ContaPagar> ContasPagar { get; set; }
+}
+
+public enum Variabilidade
+{
+    Invariavel = 0,
+    Baixa = 1,
+    Media = 2,
+    Alta = 3,
+    Altissima = 4
 }
