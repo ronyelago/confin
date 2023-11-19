cf5f299bb01bd45fefb9d6025d5356d30b8e688f
Ronye Cornetti <ronye.rocha@gmail.com>
2023-11-19 15:00:36 -0300

    refactor

--- Confin.Domain/Entities/Compra.cs
+++ Confin.Domain/Entities/Compra.cs
@@ -1,32 +1,30 @@
 ﻿using System.ComponentModel;
 
-namespace Confin.Domain.Entities
+namespace Confin.Domain.Entities;
+public record Compra
 {
-    public record Compra
-    {
-        public int Id { get; set; }
-        public string Descricao { get; set; }
-        public decimal Valor { get; set; }
-        public FormaPagamento FormaPagamento { get; set; }
-        public bool Parcelada { get; set; }
-        public DateTime DataCompra { get; set; }
-    }
+    public int Id { get; set; }
+    public string Descricao { get; set; }
+    public decimal Valor { get; set; }
+    public FormaPagamento FormaPagamento { get; set; }
+    public bool Parcelada { get; set; }
+    public DateTime DataCompra { get; set; }
+}
 
-    public enum FormaPagamento
-    {
-        [Description("Crédito")]
-        Credito = 1,
+public enum FormaPagamento
+{
+    [Description("Crédito")]
+    Credito = 1,
 
-        [Description("Débito")]
-        Debito = 2,
+    [Description("Débito")]
+    Debito = 2,
 
-        [Description("Pix")]
-        Pix = 3,
+    [Description("Pix")]
+    Pix = 3,
 
-        [Description("Crédito Parcelado")]
-        CreditoParcelado = 4,
+    [Description("Crédito Parcelado")]
+    CreditoParcelado = 4,
 
-        [Description("À Vista")]
-        Dinheiro = 5
-    }
+    [Description("À Vista")]
+    Dinheiro = 5
 }
