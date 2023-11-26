0a70368811d718f045b9868f07e023fd01f16297
Ronye Cornetti <ronye.rocha@gmail.com>
2023-11-26 15:00:44 -0300

    refactor

--- Confin.Domain/Entities/CartaoCredito.cs
+++ Confin.Domain/Entities/CartaoCredito.cs
@@ -1,13 +1,11 @@
-﻿namespace Confin.Domain.Entities
+﻿namespace Confin.Domain.Entities;
+public class CartaoCredito
 {
-    public class CartaoCredito
-    {
-        public int Id { get; set; }
-        public string Nome { get; set; }
-        public decimal FaturaAtual { get; set; }
-        public decimal Limite { get; set; }
-        public decimal DividaTotal { get; set; }
-        public int ContaId { get; set; }
-        public Conta? Conta { get; set; }
-    }
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal FaturaAtual { get; set; }
+    public decimal Limite { get; set; }
+    public decimal DividaTotal { get; set; }
+    public int ContaId { get; set; }
+    public Conta? Conta { get; set; }
 }
