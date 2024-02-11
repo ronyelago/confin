9262efcc494ab0d31965b23123d1312bc4cff174
Ronye Cornetti <ronye.rocha@gmail.com>
2024-02-11 16:00:52 -0300

    refatoration bro

--- confin.api/validators/NovaCompraValidator.cs
+++ confin.api/validators/NovaCompraValidator.cs
@@ -1,18 +1,17 @@
 using confin.api.models;
 using FluentValidation;
 
-namespace confin.api.validators
-{
-    public class NovaCompraValidator : AbstractValidator<NovaCompraModel>
-    {
-        public NovaCompraValidator()
-        {
-            RuleLevelCascadeMode = CascadeMode.Stop;
+namespace confin.api.validators;
 
-            RuleFor(x => x.Descricao).NotEmpty().MinimumLength(3).MaximumLength(64);
-            RuleFor(x => x.Valor).NotEmpty().GreaterThan(0);
-            RuleFor(x => x.FormaPagamento).NotEmpty();
-            RuleFor(x => x.DataCompra).LessThanOrEqualTo(DateTime.Now);
-        }
+public class NovaCompraValidator : AbstractValidator<NovaCompraModel>
+{
+    public NovaCompraValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(x => x.Descricao).NotEmpty().MinimumLength(3).MaximumLength(64);
+        RuleFor(x => x.Valor).NotEmpty().GreaterThan(0);
+        RuleFor(x => x.FormaPagamento).NotEmpty();
+        RuleFor(x => x.DataCompra).LessThanOrEqualTo(DateTime.Now);
     }
 }
