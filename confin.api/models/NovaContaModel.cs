367f9fd508558af17e54deb9966fb57a794ea9e7
Ronye Cornetti <ronye.rocha@gmail.com>
2024-02-14 16:00:47 -0300

    work in progress do wanderson

--- confin.api/models/NovaContaModel.cs
+++ confin.api/models/NovaContaModel.cs
@@ -1,15 +1,14 @@
 using Confin.Domain.Entities;
 
-namespace confin.api.models
+namespace confin.api.models;
+
+public class NovaContaModel
 {
-    public class NovaContaModel
-    {
-        public string? Descricao { get; set; }
-        public Variabilidade Variabilidade { get; set; }
-        public bool Ativa { get; set; }
-        public string? Observacoes { get; set; }
-        public int DiaVencimento { get; set; }
-        public DateTime DataCadastro { get; set; }
-        public DateTime DataExpiracao { get; set; }
-    }
+    public string? Descricao { get; set; }
+    public Variabilidade Variabilidade { get; set; }
+    public bool Ativa { get; set; }
+    public string? Observacoes { get; set; }
+    public int DiaVencimento { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public DateTime DataExpiracao { get; set; }
 }
