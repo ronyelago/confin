862681732bbe65758b1f7778a9d55686abbc9899
Ronye Cornetti <ronye.rocha@gmail.com>
2024-06-26 15:00:31 -0300

    refactor

--- confin.api/data/DbSession.cs
+++ confin.api/data/DbSession.cs
@@ -3,49 +3,47 @@ using Dapper;
 using Npgsql;
 using System.Data;
 
-namespace confin.data
+namespace confin.data;
+public sealed class DbSession : IDbSession, IDisposable
 {
-    public sealed class DbSession : IDbSession, IDisposable
+    private Guid _id;
+    private readonly IConfiguration _configuration;
+    public IDbConnection Connection { get; }
+    public IDbTransaction Transaction { get; set; }
+
+    public DbSession(IConfiguration configuration)
     {
-        private Guid _id;
-        private readonly IConfiguration _configuration;
-        public IDbConnection Connection { get; }
-        public IDbTransaction Transaction { get; set; }
-
-        public DbSession(IConfiguration configuration)
-        {
-            _id = Guid.NewGuid();
-            _configuration = configuration;
-            Connection = new NpgsqlConnection(_configuration.GetConnectionString("Finacon"));
-        }
-
-        public async Task<T> GetAsync<T>(string command, object parms)
-        {
-            return (await Connection.QueryAsync<T>(command, parms)
-                        .ConfigureAwait(false))
-                        .FirstOrDefault();
-        }
-
-        public async Task<List<T>> GetAll<T>(string command, object parms)
-        {
-            return (await Connection.QueryAsync<T>(command, parms)).ToList();
-        }
-
-        public async Task<T> Insert<T>(string command, object parms)
-        {
-            T result;
-            result = Connection.Query<T>(command, parms, transaction: null, commandTimeout: 60, commandType: CommandType.Text).FirstOrDefault();
-
-            return result;
-        }
-
-        public async Task<T> Update<T>(string command, object parms)
-        {
-            T result;
-            result =  Connection.Query<T>(command, parms, transaction: null, commandTimeout:60,commandType :CommandType.Text).FirstOrDefault();
-            return result;
-        }
-
-        public void Dispose() => Connection?.Dispose();
+        _id = Guid.NewGuid();
+        _configuration = configuration;
+        Connection = new NpgsqlConnection(_configuration.GetConnectionString("Finacon"));
     }
+
+    public async Task<T> GetAsync<T>(string command, object parms)
+    {
+        return (await Connection.QueryAsync<T>(command, parms)
+                    .ConfigureAwait(false))
+                    .FirstOrDefault();
+    }
+
+    public async Task<List<T>> GetAll<T>(string command, object parms)
+    {
+        return (await Connection.QueryAsync<T>(command, parms)).ToList();
+    }
+
+    public async Task<T> Insert<T>(string command, object parms)
+    {
+        T result;
+        result = Connection.Query<T>(command, parms, transaction: null, commandTimeout: 60, commandType: CommandType.Text).FirstOrDefault();
+
+        return result;
+    }
+
+    public async Task<T> Update<T>(string command, object parms)
+    {
+        T result;
+        result =  Connection.Query<T>(command, parms, transaction: null, commandTimeout:60,commandType :CommandType.Text).FirstOrDefault();
+        return result;
+    }
+
+    public void Dispose() => Connection?.Dispose();
 }
