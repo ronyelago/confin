8a1923a0fb5ae144aa754bdc1c24c6b384b0fda8
Ronye Cornetti <ronye.rocha@gmail.com>
2024-07-11 13:36:28 -0300

    refactor

--- confin.api/interfaces/Repositories/IDbSession.cs
+++ confin.api/interfaces/Repositories/IDbSession.cs
@@ -1,10 +1,8 @@
-namespace confin.api.interfaces.repositories
+namespace confin.api.interfaces.repositories;
+public interface IDbSession
 {
-    public interface IDbSession
-    {
-        Task<T> GetAsync<T> (string command, object parms);
-        Task<List<T>> GetAll<T> (string command, object parms);
-        Task<T> Insert<T> (string command, object parmms);
-        Task<T> Update<T> (string command, object parms);
-    }
+    Task<T> GetAsync<T> (string command, object parms);
+    Task<List<T>> GetAll<T> (string command, object parms);
+    Task<T> Insert<T> (string command, object parmms);
+    Task<T> Update<T> (string command, object parms);
 }
