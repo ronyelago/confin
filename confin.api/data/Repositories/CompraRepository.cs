7b704a56c0bf0c0cec5ae93980ce04793286b2dd
Ronye Cornetti <ronye.rocha@gmail.com>
2024-07-10 23:29:48 -0300

    reffactor row

--- confin.api/data/Repositories/CompraRepository.cs
+++ confin.api/data/Repositories/CompraRepository.cs
@@ -3,46 +3,45 @@ using confin.api.interfaces.repositories;
 using Dapper;
 using Confin.Domain.Entities;
 
-namespace confin.data.Repositories
+namespace confin.data.Repositories;
+public class CompraRepository : ICompraRepository
 {
-    public class CompraRepository : ICompraRepository
+    private DbSession _session;
+
+    public CompraRepository(DbSession session)
     {
-        private DbSession _session;
+        _session = session;
+    }
 
-        public CompraRepository(DbSession session)
-        {
-            _session = session;
-        }
+    public async Task<IReadOnlyList<Compra>> GetAllAsync()
+    {
+        const string query = @"SELECT id, descricao, valor, formaPagamento, parcelada, dataCompra FROM compra";
 
-        public async Task<IReadOnlyList<Compra>> GetAllAsync()
-        {
-            const string query = @"SELECT id, descricao, valor, formaPagamento, parcelada, dataCompra FROM compra";
+        var result = await _session.Connection.QueryAsync<Compra>(query, null, _session.Transaction);
 
-            var result = await _session.Connection.QueryAsync<Compra>(query, null, _session.Transaction);
+        return result.ToList();
+    }
 
-            return result.ToList();
-        }
+    public async Task<string> AddAsync(Compra compra)
+    {
+        string query = $@"INSERT INTO compra(
+                              descricao, valor, formaPagamento, parcelada, dataCompra) 
+                          VALUES(
+                              @Descricao,
+                              @Valor,
+                              @FormaPagamento,
+                              @Parcelada,
+                              NOW())";
 
-        public async Task<string> AddAsync(Compra compra)
-        {
-            string query = $@"INSERT INTO compra(
-                                  descricao, valor, formaPagamento, parcelada, dataCompra) 
-                              VALUES(
-                                  @Descricao,
-                                  @Valor,
-                                  @FormaPagamento,
-                                  @Parcelada,
-                                  NOW())";
+        DynamicParameters parameters = new DynamicParameters();
+        parameters.Add("Descricao", compra.Descricao, DbType.String);
+        parameters.Add("Valor", compra.Valor, DbType.Decimal);
+        parameters.Add("FormaPagamento", compra.FormaPagamento, DbType.Int16);
+        parameters.Add("Parcelada", compra.Parcelada, DbType.Boolean);
 
-            DynamicParameters parameters = new DynamicParameters();
-            parameters.Add("Descricao", compra.Descricao, DbType.String);
-            parameters.Add("Valor", compra.Valor, DbType.Decimal);
-            parameters.Add("FormaPagamento", compra.FormaPagamento, DbType.Int16);
-            parameters.Add("Parcelada", compra.Parcelada, DbType.Boolean);
+        var result = await _session.Connection.ExecuteAsync(query, parameters);
 
-            var result = await _session.Connection.ExecuteAsync(query, parameters);
-
-            return result.ToString();
-        }
+        return result.ToString();
     }
 }
+
