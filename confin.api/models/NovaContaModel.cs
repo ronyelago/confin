5893a0c10f4ab8b905836a23058a762a7c5a2c46
Ronye <ronye.rocha@gmail.com>
2024-12-18 14:21:03 -0300

    adjusts

--- confin.api/models/NovaContaModel.cs
+++ confin.api/models/NovaContaModel.cs
@@ -5,10 +5,10 @@ namespace confin.api.models;
 
 public class NovaContaModel
 {
-    public string? Descricao { get; set; }
+    public string Descricao { get; set; }
     public Variabilidade Variabilidade { get; set; }
     public bool Ativa { get; set; }
-    public string? Observacoes { get; set; }
+    public string Observacoes { get; set; }
     public int DiaVencimento { get; set; }
     public DateTime DataCadastro { get; set; }
     public DateTime DataExpiracao { get; set; }
