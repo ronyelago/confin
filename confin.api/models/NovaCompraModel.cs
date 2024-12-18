e206035e7fe8805b608b7859fdea65595567694b
Ronye <ronye.rocha@gmail.com>
2024-12-18 14:21:28 -0300

    adjusts

--- confin.api/models/NovaCompraModel.cs
+++ confin.api/models/NovaCompraModel.cs
@@ -5,7 +5,7 @@ namespace confin.api.models;
 
 public class NovaCompraModel
 {
-    public string? Descricao { get; set; }
+    public string Descricao { get; set; }
     public decimal Valor { get; set; }
     public bool Parcelada { get; set; }
     public FormaPagamento FormaPagamento { get; set; }
